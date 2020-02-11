689eb4562cefe73d555aa59f91eb3685211dcf28
Naxesss <naxess.w@gmail.com>
2020-02-11 22:16:50 +0100

    Add parser specific exceptions

---
+++ src/parsers/exceptions.py
@@ -0,0 +1,10 @@
+
+
+class ParsingError(Exception):
+    """For use in cases where parsing some portion of an event failed and should stop the program."""
+    pass
+
+class DeletedContextError(Exception):
+    """For use in cases where some portion of an event is missing due to its context being deleted.
+    This should not stop the program, but instead simply delete the event the error was raised from."""
+    pass
