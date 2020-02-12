c3052d4a225848dd38368e6f5a2e3ad99422f851
Naxesss <naxess.w@gmail.com>
2020-02-12 12:05:27 +0100

    Add caching for api calls

--- src/web/api.py
+++ src/web/api.py
@@ -1,4 +1,5 @@
 import json
+from collections import defaultdict
 
 from web.ratelimiter import request_with_rate_limit
 from storage.settings import API_KEY, API_RATE_LIMIT
@@ -9,15 +10,30 @@ def request_api(request_type: str, query: str) -> object:
     response = request_with_rate_limit(request, API_RATE_LIMIT, "api")
     return json.loads(response.text)
 
+requested_beatmapsets = defaultdict()
 def request_beatmapset(beatmapset_id: str) -> object:
-    """Requests a json object of the given beatmapset id."""
-    return request_api("get_beatmaps", f"s={beatmapset_id}")
+    """Requests a json object of the given beatmapset id.
+    Caches any response gotten until cleared manually."""
+    if beatmapset_id in requested_beatmapsets:
+        return requested_beatmapsets[beatmapset_id]
+    
+    beatmapset_json = request_api("get_beatmaps", f"s={beatmapset_id}")
+    requested_beatmapsets[beatmapset_id] = beatmapset_json
+    return beatmapset_json
 
+requested_users = defaultdict()
 def request_user(user_id: str) -> object:
-    """Requests a json object of the given user id."""
-    userObj = request_api("get_user", f"u={user_id}")
-    if len(userObj) > 0:
-        return userObj[0]
+    """Requests a json object of the given user id.
+    Caches any response gotten until cleared manually."""
+
+    if user_id in requested_users:
+        return requested_users[user_id]
+    
+    user_json = request_api("get_user", f"u={user_id}")
+    if len(user_json) > 0:
+        requested_users[user_id] = user_json
+        return user_json[0]
     else:
         # For when the user does not exist (e.g. restricted).
+        # Do not cache, in case they get unrestricted soon.
         return None
