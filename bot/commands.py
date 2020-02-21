3cfcd83001cb60f0f6793ef98361294bc868ac8b
Naxesss <naxess.w@gmail.com>
2020-02-21 21:53:37 +0100

    Add __str__ and __eq__ overrides to Command

--- bot/commands.py
+++ bot/commands.py
@@ -3,3 +3,15 @@ class Command():
     def __init__(self, name: str, *args: str):
         self.name = name
         self.args = [arg for arg in args]
+    
+    def __str__(self) -> str:
+        args = [f" {arg}" for arg in self.args]
+        return f"+{self.name}{args}"
+    
+    def __eq__(self, other) -> bool:
+        if type(self) != type(other):
+            return False
+        return (
+            self.name == other.name and
+            self.args == other.args
+        )
