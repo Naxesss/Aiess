ec5978882e5b37fb8fea2aef377e26eab22d829a
Naxesss <naxess.w@gmail.com>
2020-02-21 19:56:12 +0100

    Abstract message received

--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,13 @@
 import discord
+from discord import Message
 
 from settings import API_KEY
 
 class Client(discord.Client):
-    async def on_ready(self):
+    async def on_ready(self) -> None:
         print(f"Logged on as {self.user}!")
 
-    async def on_message(self, message):
+    async def on_message(self, message: Message) -> None:
         print(f"Message from {message.author}: {message.content}")
 
 client = Client()
