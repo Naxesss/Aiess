08ac16801a341eb619d2c474a557b98d70b73bf4
Naxesss <naxess.w@gmail.com>
2020-02-21 21:50:50 +0100

    Add receiver tests

---
+++ bot/tests/test_receiver.py
@@ -0,0 +1,15 @@
+
+from commands import Command
+from receiver import parse_command, receive_command
+
+def test_find_command():
+    assert parse_command("+test") == Command("test")
+    assert parse_command("+test 123") == Command("test", "123")
+    assert parse_command("+test 1 2 3") == Command("test", "1", "2", "3")
+    assert not parse_command("123")
+    assert not parse_command("+ test")
+    assert not parse_command("++test")
+    assert not parse_command("123 +test")
+
+def test_receive_command():
+    assert receive_command(Command("ping"))
