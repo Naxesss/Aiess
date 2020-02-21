c35885473f4500035ece24c78a4a7450a2e6fb5e
Naxesss <naxess.w@gmail.com>
2020-02-21 21:50:46 +0100

    Add command tests

---
+++ bot/tests/commands/test_command.py
@@ -0,0 +1,13 @@
+from commands import Command
+
+def test_init():
+    command = Command("test", "1", "2", "3")
+    
+    assert command.name == "test"
+    assert command.args == ["1", "2", "3"]
+
+def test_init_no_args():
+    command = Command("test")
+
+    assert command.name == "test"
+    assert not command.args
