f7a79f276bb3b54c11740fe37304a8d8ca717369
Naxesss <naxess.w@gmail.com>
2020-02-21 20:19:54 +0100

    Add placeholder command handling methods

--- bot/receiver.py
+++ bot/receiver.py
@@ -2,3 +2,11 @@ from discord import Message
 
 def receive(message: Message) -> None:
     print(f"({message.guild} > #{message.channel}) {message.author}: {message.content}")
+    if qualifies_as_command(message):
+        receive_command(message)
+
+def qualifies_as_command(message: Message) -> None:
+    pass
+
+def receive_command(command: str) -> None:
+    pass
