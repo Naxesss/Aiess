dfdc85d5d9b718cb0fbedfad4f428dedb4438b3d
Naxesss <naxess.w@gmail.com>
2020-02-22 19:19:13 +0100

    Add a MockCommand class for testing

---
+++ bot/tests/mock_command.py
@@ -0,0 +1,14 @@
+from commands import Command
+
+class MockCommand(Command):
+    """Represents the values with which a command is called (i.e. name, args, context),
+    as well as output from the command (e.g. response)."""
+    def __init__(self, name: str, *args: str):
+        super().__init__(name, args)
+        self.response = None
+
+    async def respond(self, response: str) -> bool:
+        """A mocked version of response, always returns true.
+        Sets the response attribute to the given response for checking."""
+        self.response = response
+        return True
