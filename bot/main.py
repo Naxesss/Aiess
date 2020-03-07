4dcd8e9945468e26f0d69053a8c8dc65ddb310a5
Naxesss <naxess.w@gmail.com>
2020-03-07 10:30:28 +0100

    Add implementation of Reader in bot

--- bot/main.py
+++ bot/main.py
@@ -4,6 +4,13 @@ from discord import Message
 import receiver
 from settings import API_KEY
 
+import aiess
+from aiess import Event
+
+class Reader(aiess.Reader):
+    async def on_event(self, event: Event):
+        pass
+
 class Client(discord.Client):
     async def on_ready(self) -> None:
         print(f"Logged on as {self.user}!")
