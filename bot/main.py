976437f0af7526e3abd62c1fb779ad3c5d1d9064
Naxesss <naxess.w@gmail.com>
2020-03-13 13:17:30 +0100

    Run reader in bot on_ready

--- bot/main.py
+++ bot/main.py
@@ -11,9 +11,15 @@ class Reader(aiess.Reader):
     async def on_event(self, event: Event):
         pass
 
+reader = Reader("bot")
+
 class Client(discord.Client):
     async def on_ready(self) -> None:
         print(f"Logged on as {self.user}!")
+
+        global reader
+        if not reader.running:
+            await reader.run()
 
     async def on_message(self, message: Message) -> None:
         await receiver.receive(message)
