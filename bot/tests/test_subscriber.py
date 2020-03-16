98787db804fdc385163abadafed6c6eae6662d5f
Naxesss <naxess.w@gmail.com>
2020-03-17 00:30:19 +0100

    Add a test for forwarding, along with a client mock
    Currently fails due to filtering not being implemented.

--- bot/tests/test_subscriber.py
+++ bot/tests/test_subscriber.py
@@ -1,4 +1,7 @@
 import pytest
+from datetime import datetime
+
+from aiess import Event
 
 import subscriber
 from subscriptions import Subscription
@@ -47,3 +50,31 @@ def test_remove_subscription(test_database: Database):
 
     subscriber.remove_subscription(sub1, test_database)
     assert sub1 not in subscriber.cache
+
+class MockClient():
+    def __init__(self):
+        self.event_sub_pairs = []
+
+    async def send_event(self, event: Event, sub: Subscription) -> None:
+        self.event_sub_pairs.append((sub, event))
+
+@pytest.mark.asyncio
+async def test_forward(test_database: Database):
+    sub_both = Subscription(guild_id=1, channel_id=2, _filter="type:test1 or type:test2")
+    sub_one = Subscription(guild_id=1, channel_id=1, _filter="type:test1")
+
+    subscriber.add_subscription(sub_both, test_database)
+    subscriber.add_subscription(sub_one, test_database)
+
+    event1 = Event(_type="test1", time=datetime.utcnow())
+    event2 = Event(_type="test2", time=datetime.utcnow())
+    client = MockClient()
+
+    await subscriber.forward(event1, client)
+    await subscriber.forward(event2, client)
+
+    assert (sub_both, event1) in client.event_sub_pairs
+    assert (sub_both, event2) in client.event_sub_pairs
+
+    assert (sub_one, event1) in client.event_sub_pairs
+    assert (sub_one, event2) not in client.event_sub_pairs, "A subscription was forwarded an event it was supposed to filter."
