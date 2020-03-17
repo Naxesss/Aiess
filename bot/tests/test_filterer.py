3b69500a357af38a521514d81a43f94e02645f91
Naxesss <naxess.w@gmail.com>
2020-03-17 18:23:00 +0100

    Finish adding filterer tests

--- bot/tests/test_filterer.py
+++ bot/tests/test_filterer.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from aiess import Event, Beatmapset, User
+from aiess import Event, User, Beatmapset, Discussion
 
 from filterer import expand, dissect
 
@@ -31,8 +31,73 @@ def test_dissect_simple():
     event = Event(_type="test", time=datetime.utcnow())
     assert dissect(event) == ["type:test"]
 
+def test_dissect_user():
+    user = User(2, "some two")
+    event = Event(_type="test", time=datetime.utcnow(), user=user)
+    assert dissect(user) == [
+        "user:\"some two\"",
+        "user-id:2"
+    ]
+    assert dissect(event) == ["type:test"] + dissect(user)
+
 def test_dissect_beatmapset():
     user = User(2, "some two")
-    beatmapset = Beatmapset(4, artist="", title="", creator=user, modes=["osu", "catch"])
+    beatmapset = Beatmapset(4, artist="yes", title="no", creator=user, modes=["osu", "catch"])
     event = Event(_type="test", time=datetime.utcnow(), beatmapset=beatmapset)
-    assert dissect(event) == ["type:test"]
+    assert dissect(beatmapset) == [
+        "mapset-id:4",
+        "artist:yes",
+        "title:no",
+        "creator:\"some two\"",
+        "creator-id:2",
+        "mode:osu",
+        "mode:catch"
+    ]
+    assert dissect(event) == ["type:test"] + dissect(beatmapset)
+
+def test_dissect_discussion():
+    user = User(1, "some one")
+    creator = User(2, "some two")
+    beatmapset = Beatmapset(4, artist="yes", title="no", creator=creator, modes=["osu", "catch"])
+    discussion = Discussion(3, beatmapset=beatmapset, user=user, content="hello")
+    event = Event(_type="test", time=datetime.utcnow(), beatmapset=beatmapset, discussion=discussion, user=user, content="hello")
+    assert dissect(beatmapset) == [
+        "mapset-id:4",
+        "artist:yes",
+        "title:no",
+        "creator:\"some two\"",
+        "creator-id:2",
+        "mode:osu",
+        "mode:catch"
+    ]
+    assert dissect(discussion) == dissect(beatmapset) + [
+        "discussion-id:3",
+        "author:\"some one\"",
+        "author-id:1",
+        "discussion-content:hello"
+    ]
+    assert dissect(event) == ["type:test"] + dissect(discussion) + dissect(user) + ["content:hello"]
+
+def test_dissect_discussion_reply():
+    user = User(1, "some one")
+    creator = User(2, "some two")
+    replier = User(3, "some three")
+    beatmapset = Beatmapset(4, artist="yes", title="no", creator=creator, modes=["osu", "catch"])
+    discussion = Discussion(3, beatmapset=beatmapset, user=user, content="hello")
+    event = Event(_type="reply", time=datetime.utcnow(), beatmapset=beatmapset, discussion=discussion, user=replier, content="there")
+    assert dissect(beatmapset) == [
+        "mapset-id:4",
+        "artist:yes",
+        "title:no",
+        "creator:\"some two\"",
+        "creator-id:2",
+        "mode:osu",
+        "mode:catch"
+    ]
+    assert dissect(discussion) == dissect(beatmapset) + [
+        "discussion-id:3",
+        "author:\"some one\"",
+        "author-id:1",
+        "discussion-content:hello"
+    ]
+    assert dissect(event) == ["type:reply"] + dissect(discussion) + dissect(replier) + ["content:there"]
