47e3d27bb9f1895848620c5ff88969894d8b8c5a
Naxesss <naxess.w@gmail.com>
2020-04-06 01:01:00 +0200

    Remove duplicate test case

--- bot/tests/test_filterer.py
+++ bot/tests/test_filterer.py
@@ -148,9 +148,6 @@ def test_de_morgans_law_elimination():
 def test_de_morgans_law_negated_word():
     assert de_morgans_law("not type:(nominate or qualify)") == "not type:nominate and not type:qualify"
 
-def test_de_morgans_law_negated_word():
-    assert de_morgans_law("not type:(nominate or qualify)") == "type:(not nominate and not qualify)"
-
 def test_negate():
     assert negate("A | B & C") == "!A & (!B | !C)"
 
