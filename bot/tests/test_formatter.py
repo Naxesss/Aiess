e1013b445ea0b1a997f77df80e049f2b18eed551
Naxesss <naxess.w@gmail.com>
2020-04-12 11:50:41 +0200

    Add additional formatting test cases

--- bot/tests/test_formatter.py
+++ bot/tests/test_formatter.py
@@ -21,17 +21,19 @@ def suggestion_event():
 
     return event
 
+@pytest.fixture
+def qualify_event():
+    beatmapset = Beatmapset(3, "artist_with*strange~symbols`", "title", User(2, "_sometwo_"), ["osu"])
+    event = Event("qualify", from_string("2020-04-11 20:00:00"), beatmapset)
+
+    return event
 
 
 def test_format_link_discussion(suggestion_event):
     assert format_link(suggestion_event) == "https://osu.ppy.sh/beatmapsets/3/discussion#/5"
 
-def test_format_link_no_discussion():
-    beatmapset = Beatmapset(3, "artist", "title", User(2, "sometwo"), ["osu"])
-    user = User(1, "someone")
-    event = Event("nominate", from_string("2020-04-11 20:00:00"), beatmapset, user=user)
-
-    assert format_link(event) == "https://osu.ppy.sh/beatmapsets/3"
+def test_format_link_no_discussion(qualify_event):
+    assert format_link(qualify_event) == "https://osu.ppy.sh/beatmapsets/3"
 
 def test_format_link_no_beatmapset():
     event = Event("test", from_string("2020-04-11 20:00:00"))
@@ -57,15 +59,28 @@ def test_format_embed(suggestion_event):
 def test_format_field_name(suggestion_event):
     assert format_field_name(suggestion_event) == ":yellow_circle: Suggestion"
 
+def test_format_field_name_qualify(qualify_event):
+    assert format_field_name(qualify_event) == ":heart: Qualified"
+
 def test_format_field_value(suggestion_event):
     assert (format_field_value(suggestion_event) ==
         "[**artist - title**](https://osu.ppy.sh/beatmapsets/3)\nMapped by [sometwo](https://osu.ppy.sh/users/2) [**osu**]")
 
+def test_format_field_value_markdown(qualify_event):
+    assert (format_field_value(qualify_event) ==
+        "[**artist\\_with\\*strange\\~symbols\\` - title**](https://osu.ppy.sh/beatmapsets/3)\nMapped by [\\_sometwo\\_](https://osu.ppy.sh/users/2) [**osu**]")
+
 def test_format_footer_text(suggestion_event):
     assert format_footer_text(suggestion_event) == "someone \"hi\""
 
+def test_format_footer_text_no_user(qualify_event):
+    assert format_footer_text(qualify_event) == Embed.Empty
+
 def test_format_footer_icon_url(suggestion_event):
     assert format_footer_icon_url(suggestion_event) == "https://a.ppy.sh/1"
+
+def test_format_footer_icon_url_no_user(qualify_event):
+    assert format_footer_icon_url(qualify_event) == Embed.Empty
 
 def test_format_thumbnail_url(suggestion_event):
     assert format_thumbnail_url(suggestion_event) == "https://b.ppy.sh/thumb/3l.jpg"
