c159e57cbaa8859e9c41ac338db7c2d93e6f7938
Naxesss <naxess.w@gmail.com>
2020-04-12 08:27:09 +0200

    Add documentation to formatting functions

--- bot/formatter.py
+++ bot/formatter.py
@@ -8,6 +8,8 @@ type_field_name = {
 }
 
 def format_link(event: Event) -> str:
+    """Returns a link which leads to the source of the given event
+    (e.g. the discussion of a suggestion, or the beatmapset of a nomination)."""
     if event.discussion and event.discussion.beatmapset:
         return f"https://osu.ppy.sh/beatmapsets/{event.discussion.beatmapset.id}/discussion#/{event.discussion.id}"
     
@@ -17,6 +19,7 @@ def format_link(event: Event) -> str:
     raise ValueError("Cannot format a link of an event missing a beatmapset.")
 
 def format_embed(event: Event) -> str:
+    """Returns an embed which represents the given event."""
     embed = Embed()
     embed.add_field(name=format_field_name(event), value=format_field_value(event), inline=False)
     embed.set_footer(text=format_footer_text(event), icon_url=format_footer_icon_url(event))
@@ -25,18 +28,21 @@ def format_embed(event: Event) -> str:
 
 
 
-def escape_markdown(string: str) -> str:
-    # Cast to str in case the argument is of type e.g. User.
-    return (str(string)
+def escape_markdown(obj: str) -> str:
+    """Returns the given object cast to string, and where all markdown
+    markers (e.g. '*', '_', and '~' characters) are escaped using backslash."""
+    return (str(obj)
         .replace("*", "\\*")   # Italic / Bold
         .replace("_", "\\_")   # Italic / Underline
         .replace("~", "\\~")   # Strikethrough
         .replace("`", "\\`"))  # Code / Code Block
 
 def format_field_name(event: Event) -> str:
+    """Returns the embed title of the given event (e.g. :heart: Qualified)."""
     return type_field_name[event.type]
 
 def format_field_value(event: Event) -> str:
+    """Returns the embed contents of the given event (i.e. the \"artist - title, mapped by creator [modes]\" part)."""
     artist_title_str = escape_markdown(f"{event.beatmapset.artist} - {event.beatmapset.title}")
     creator_str = escape_markdown(event.beatmapset.creator)
 
@@ -47,6 +53,8 @@ def format_field_value(event: Event) -> str:
     return f"{beatmap_str}\n{mapped_by_str} {mode_str}"
 
 def format_footer_text(event: Event) -> str:
+    """Returns the footer text of the event (e.g. modder \"00:01:318 - fix blanket\"),
+    if there's a user associated with the event, otherwise None."""
     if event.user:
         if event.content:
             # Markdown does not function in footer text; trying to escape it here would lead to visible '\'.
@@ -55,6 +63,8 @@ def format_footer_text(event: Event) -> str:
     return None
 
 def format_footer_icon_url(event: Event) -> str:
+    """Returns the footer icon url of the event (i.e. the image url of the user's avatar),
+    if there's a user associated with the event, otherwise None."""
     if event.user:
         return f"https://a.ppy.sh/{event.user.id}"
     return None
