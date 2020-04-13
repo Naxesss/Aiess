89e929b2d5a4d407bb58778c7f39e08a01611127
Naxesss <naxess.w@gmail.com>
2020-04-13 12:16:41 +0200

    Add a few TODOs for the sub command
    These are things that should definitely be done before adding help commands.
    
    The latter ones are especially critical, as inputting an invalid filter currently causes the program to raise on startup, with no feedback that anything went wrong.

--- bot/cmd_modules/cmd_sub.py
+++ bot/cmd_modules/cmd_sub.py
@@ -8,7 +8,11 @@ async def cmd_ping(command: Command):
         return
     
     # We only take one argument, so let's combine all whitespace splits back together.
+    # TODO: Make this implicit by registering the argument amount, after which whitespace no longer splits.
     combined_arg = " ".join(command.args)
+
+    # TODO: Ensure our filter argument is actually a valid filter (e.g. doesn't throw on parenthesis inequality).
+    # TODO: Improve feedback by ecapsulating our input and its expansion in an embed.
 
     subscribe(command.context.channel, combined_arg)
     await command.respond("✓ subbed")
