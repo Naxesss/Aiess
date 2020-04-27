ed98564e7983452c1ef48569dcb36ea5d0e73b4f
Naxesss <naxess.w@gmail.com>
2020-04-27 11:17:53 +0200

    Add tests for retrieving beatmapset events

--- bot/tests/test_database.py
+++ bot/tests/test_database.py
@@ -1,6 +1,9 @@
 import pytest
 from mysql.connector.errors import IntegrityError
 
+from aiess import Event, User, Beatmapset
+from aiess.timestamp import from_string
+
 from database import Database
 from subscriptions import Subscription
 
@@ -41,3 +44,34 @@ def test_insert_retrieve_channel_sub_no_filter(test_bot_database):
         test_bot_database.insert_subscription(sub)
     
     assert "cannot be null" in str(err)
+
+def test_retrieve_beatmapset_events(test_database):
+    beatmapset = Beatmapset(3, "artist", "title", User(4, "creator"), ["osu"])
+    nom_event = Event("nominate", from_string("2020-01-01 00:00:00"), beatmapset, user=User(1, "someone"))
+    qual_event = Event("qualify", from_string("2020-01-01 05:00:00"), beatmapset, user=User(2, "sometwo"))
+    suggestion_event = Event("suggestion", from_string("2020-01-01 01:00:00"), beatmapset, user=User(3, "somethree"))
+
+    test_database.insert_event(suggestion_event)
+    test_database.insert_event(nom_event)
+    test_database.insert_event(qual_event)
+    
+    events = test_database.retrieve_beatmapset_events(beatmapset)
+    assert nom_event in events
+    assert qual_event in events
+    assert suggestion_event in events
+
+def test_retrieve_beatmapset_events_cache(test_database):
+    beatmapset = Beatmapset(3, "artist", "title", User(4, "creator"), ["osu"])
+    nom_event = Event("nominate", from_string("2020-01-01 00:00:00"), beatmapset, user=User(1, "someone"))
+    qual_event = Event("qualify", from_string("2020-01-01 05:00:00"), beatmapset, user=User(2, "sometwo"))
+
+    test_database.insert_event(nom_event)
+    test_database.insert_event(qual_event)
+    
+    test_database.retrieve_beatmapset_events(beatmapset)
+    assert test_database.beatmapset_event_cache["3"] == [qual_event, nom_event]
+
+    test_database.clear_cache()
+    assert 3 not in test_database.beatmapset_event_cache
+    assert nom_event not in test_database.beatmapset_event_cache.values()
+    assert qual_event not in test_database.beatmapset_event_cache.values()
