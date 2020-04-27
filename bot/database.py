0d124148c7bc232ed6affe2b2be5f6dbc3776ae1
Naxesss <naxess.w@gmail.com>
2020-04-27 11:21:10 +0200

    Add passing database mapset events methods
    The use of a cache speeds up performance, as accessing an element in a dictionary is O(1), while accessing elements in the database is at least O(n).

--- bot/database.py
+++ bot/database.py
@@ -1,6 +1,7 @@
-from typing import Generator
+from typing import Generator, Dict, List
 
 import aiess
+from aiess import Event, Beatmapset
 
 from subscriptions import Subscription
 
@@ -8,6 +9,7 @@ class Database(aiess.Database):
     """Creates an aiess_bot database connection, with methods to insert and retrieve subscriptions."""
 
     def __init__(self, _db_name:str="aiess_bot"):
+        self.beatmapset_event_cache: Dict[int, List[Event]] = {}
         super().__init__(_db_name)
     
     def insert_subscription(self, subscription: Subscription):
@@ -34,5 +36,24 @@ class Database(aiess.Database):
         The filter of the given subscription does not matter, as there is only one in each channel anyway."""
         self.delete_table_data("subscriptions",
             where_str=f"guild_id={sub.guild_id} AND channel_id={sub.channel_id}")
+    
+    def retrieve_beatmapset_events(self, beatmapset: Beatmapset) -> List[Event]:
+        """Retrieves all events which have the given beatmapset id associated in
+        descending order (i.e. newer events first), from the database, then stores
+        the result in a cache used for any consecutive call.
+        
+        The cache must be cleared manually before new information can be obtained,
+        see `clear_cache`."""
+        if beatmapset.id not in self.beatmapset_event_cache:
+            # Casting to list generates the events so that we can cache them.
+            self.beatmapset_event_cache[beatmapset.id] = list(
+                self.retrieve_events(f"beatmapset_id = {beatmapset.id} ORDER BY time DESC"))
+
+        return self.beatmapset_event_cache[beatmapset.id]
+
+    def clear_cache(self) -> None:
+        """Clears any cache the database may be using, allowing new info to be obtained
+        (e.g. for retrieving all events related to a beatmapset)."""
+        self.beatmapset_event_cache.clear()
 
 database = Database()
