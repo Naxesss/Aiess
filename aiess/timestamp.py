5080daa24ef98c2a3e7522cafa93f5965600d958
Naxesss <naxess.w@gmail.com>
2020-05-15 09:55:48 +0200

    Change `id` args to `_id` in timestamp
    `id` is a built-in function, so we should avoid redefining it, if possible.

--- aiess/timestamp.py
+++ aiess/timestamp.py
@@ -10,17 +10,17 @@ TIME_FORMAT_ALT = "%Y-%m-%dT%H:%M:%S+00:00"
 FILE_NAME_PREFIX = "last_datetime-"
 FILE_NAME_POSTFIX = ".txt"
 
-def get_last(id: str=None) -> datetime:
+def get_last(_id: str=None) -> datetime:
     """Returns the last datetime we're done with for this id. If the file doesn't exist, one will be created
     with the current time in UTC as datetime."""
     # Only in cases where the file does not already exist do we want to create and initialize one.
     # In any other case we should throw an exception if data is missing (e.g. corruption due to power loss),
     # to prevent silent failure.
-    if not os.path.exists(get_dir_path(id)):
-        os.makedirs(get_dir_path(id))
+    if not os.path.exists(get_dir_path(_id)):
+        os.makedirs(get_dir_path(_id))
     
-    path = get_path(id)
-    if not exists(id):
+    path = get_path(_id)
+    if not exists(_id):
         with open(path, "w") as _file:
             _file.write(to_string(datetime.utcnow()))
 
@@ -33,24 +33,24 @@ def get_last(id: str=None) -> datetime:
         last_datetime = datetime.strptime(last_datetime_text, TIME_FORMAT)
         return last_datetime
 
-def set_last(new_datetime: datetime, id: str=None) -> None:
+def set_last(new_datetime: datetime, _id: str=None) -> None:
     """Sets the last datetime we're done with for this id. Creates the respective file if it does not exist."""
-    if not os.path.exists(get_dir_path(id)):
-        os.makedirs(get_dir_path(id))
+    if not os.path.exists(get_dir_path(_id)):
+        os.makedirs(get_dir_path(_id))
     
-    with open(get_path(id), "w") as _file:
+    with open(get_path(_id), "w") as _file:
         _file.write(to_string(new_datetime))
 
-def exists(id: str=None) -> bool:
+def exists(_id: str=None) -> bool:
     """Returns whether a datetime file for this id exists."""
-    return os.path.exists(get_path(id))
+    return os.path.exists(get_path(_id))
 
-def get_path(id: str=None) -> str:
+def get_path(_id: str=None) -> str:
     """Returns the path to the event time file with the given identifier."""
     global PATH_PREFIX, FILE_NAME_PREFIX, FILE_NAME_POSTFIX
-    return f"{PATH_PREFIX}{FILE_NAME_PREFIX}{id}{FILE_NAME_POSTFIX}"
+    return f"{PATH_PREFIX}{FILE_NAME_PREFIX}{_id}{FILE_NAME_POSTFIX}"
 
-def get_dir_path(id: str=None) -> str:
+def get_dir_path(_id: str=None) -> str:
     """Returns the path to the event time file with the given identifier."""
     global PATH_PREFIX
     return f"{PATH_PREFIX}"
