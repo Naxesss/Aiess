e03268d0b16f7bbaa53c50890df34426d9601c02
Naxesss <naxess.w@gmail.com>
2020-05-20 04:03:25 +0200

    Remove unused portions of mock_command.py
    None of our tests care about which message, channel, or guild the command responded to/in.
    
    If we need this, it shouldn't be difficult adding something like this back again.

--- bot/tests/commands/mock_command.py
+++ bot/tests/commands/mock_command.py
@@ -3,30 +3,11 @@ sys.path.append('..')
 
 from bot.commands import Command
 
-class MockGuild():
-    def __init__(self, _id: int):
-        self.id = _id
-
-class MockChannel():
-    def __init__(self, _id: int, guild: MockGuild):
-        self.id = _id
-        self.guild = guild
-        self.message = None
-
-    def send(self, content: str):
-        self.message.channel.command.response = content
-
-class MockMessage():
-    def __init__(self, channel: MockChannel):
-        self.channel = channel
-        self.command = None
-
 class MockCommand(Command):
     """Represents the values with which a command is called (i.e. name, args, context),
     as well as output from the command (e.g. response)."""
-    def __init__(self, name: str, *args: str, context: MockMessage=None):
-        if context: context.command = self
-        super().__init__(name, *args, context=context)
+    def __init__(self, name: str, *args: str):
+        super().__init__(name, *args)
 
         self.response = None
 
