6317a201799f84853595257bfe5be7b6155fe904
Naxesss <naxess.w@gmail.com>
2020-06-13 13:03:48 +0200

    Add argument testing for cmd_sub

--- bot/tests/commands/test_cmd_sub.py
+++ bot/tests/commands/test_cmd_sub.py
@@ -34,5 +34,77 @@ async def test_sub_no_arg():
     mock_message = MockMessage(channel=MockChannel(_id=6, guild=MockGuild(_id=2)))
     mock_command = MockCommand("sub", context=mock_message)
 
+    assert not await receive_command(mock_command)
+    assert mock_command.response.startswith("✗")
+    assert "argument" in mock_command.response.lower()
+    assert not subscriber.cache
+
+@pytest.mark.asyncio
+async def test_sub_parenthesis_inequality():
+    mock_message = MockMessage(channel=MockChannel(_id=6, guild=MockGuild(_id=2)))
+    mock_command = MockCommand("sub", "type:(nominate", context=mock_message)
+
     assert await receive_command(mock_command)
     assert mock_command.response.startswith("✗")
+    assert "parenthes" in mock_command.response.lower()
+    assert not subscriber.cache
+
+@pytest.mark.asyncio
+async def test_sub_undefined_type():
+    mock_message = MockMessage(channel=MockChannel(_id=6, guild=MockGuild(_id=2)))
+    mock_command = MockCommand("sub", "type:undefined", context=mock_message)
+
+    assert await receive_command(mock_command)
+    assert mock_command.response.startswith("✗")
+    assert "`type:undefined`" in mock_command.response.lower()
+    assert not subscriber.cache
+
+@pytest.mark.asyncio
+async def test_sub_undefined_type_duplicates():
+    mock_message = MockMessage(channel=MockChannel(_id=6, guild=MockGuild(_id=2)))
+    mock_command = MockCommand("sub", "type:undefined or type:undefined", context=mock_message)
+
+    assert await receive_command(mock_command)
+    assert mock_command.response.startswith("✗")
+    assert mock_command.response.lower().count("`type:undefined`") == 1
+    assert not subscriber.cache
+
+@pytest.mark.asyncio
+async def test_sub_undefined_key():
+    mock_message = MockMessage(channel=MockChannel(_id=6, guild=MockGuild(_id=2)))
+    mock_command = MockCommand("sub", "undefined:nominate or undefined:qualify", context=mock_message)
+
+    assert await receive_command(mock_command)
+    assert mock_command.response.startswith("✗")
+    assert mock_command.response.lower().count("`undefined`") == 1
+    assert not subscriber.cache
+
+@pytest.mark.asyncio
+async def test_sub_forgotten_colon():
+    mock_message = MockMessage(channel=MockChannel(_id=6, guild=MockGuild(_id=2)))
+    mock_command = MockCommand("sub", "typequalify and type:nominate", context=mock_message)
+
+    assert await receive_command(mock_command)
+    assert mock_command.response.startswith("✗")
+    assert "`typequalify`" in mock_command.response
+    assert not subscriber.cache
+
+@pytest.mark.asyncio
+async def test_sub_typoed_and():
+    mock_message = MockMessage(channel=MockChannel(_id=6, guild=MockGuild(_id=2)))
+    mock_command = MockCommand("sub", "type:qualify annd type:nominate", context=mock_message)
+
+    assert await receive_command(mock_command)
+    assert mock_command.response.startswith("✗")
+    assert "`annd`" in mock_command.response
+    assert not subscriber.cache
+
+@pytest.mark.asyncio
+async def test_sub_exclamation_in_value():
+    mock_message = MockMessage(channel=MockChannel(_id=6, guild=MockGuild(_id=2)))
+    mock_command = MockCommand("sub", "mode:osu or mode:osu!catch", context=mock_message)
+
+    assert await receive_command(mock_command)
+    assert mock_command.response.startswith("✗")
+    assert "`!mode`" in mock_command.response
+    assert not subscriber.cache
