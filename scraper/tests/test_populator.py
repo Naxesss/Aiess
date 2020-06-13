09f5aaf273cbe1904feb599517b1ed09b7397f61
Naxesss <naxess.w@gmail.com>
2020-06-13 18:23:54 +0200

    Add test for complete discussion context

--- scraper/tests/test_populator.py
+++ scraper/tests/test_populator.py
@@ -8,7 +8,7 @@ from aiess import Beatmapset, Discussion, User, Event
 from aiess.errors import ParsingError
 from aiess.tests.mocks.api import beatmap as mock_beatmap
 from aiess.tests.mocks.api import old_beatmap as mock_old_beatmap
-from aiess.database import SCRAPER_TEST_DB_NAME
+from aiess.database import Database, SCRAPER_TEST_DB_NAME
 from aiess.timestamp import from_string
 from aiess import event_types as types
 
@@ -43,6 +43,17 @@ def test_incomplete_context():
     discussion = Discussion(99, beatmapset)  # Missing user and content.
     
     assert not __complete_discussion_context(discussion, db_name=SCRAPER_TEST_DB_NAME)
+
+def test_complete_context():
+    beatmapset = Beatmapset(1001546, beatmapset_json=mock_beatmap.JSON)
+    discussion = Discussion(99, beatmapset, user=User(1, "someone"), content="hello there")
+    incomplete_discussion = Discussion(99, beatmapset)
+    
+    Database(SCRAPER_TEST_DB_NAME).insert_discussion(discussion)
+
+    assert __complete_discussion_context(incomplete_discussion, db_name=SCRAPER_TEST_DB_NAME)
+    assert incomplete_discussion.user
+    assert incomplete_discussion.content
 
 def test_delete_incomplete_context():
     beatmapset = Beatmapset(1001546, beatmapset_json=mock_beatmap.JSON)
