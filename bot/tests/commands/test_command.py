24550e6f498a2ecfe904c6389fb906c267266a46
Naxesss <naxess.w@gmail.com>
2020-06-21 13:00:08 +0200

    Add command help_embed test

--- bot/tests/commands/test_command.py
+++ bot/tests/commands/test_command.py
@@ -2,7 +2,9 @@ import sys
 sys.path.append('..')
 
 from bot.commands import COMMAND_PREFIX
+from bot.commands import registered_commands
 from bot.commands import Command, FunctionWrapper
+from bot.commands import help_embed
 
 def test_init_command():
     command = Command("test", "1", "2", "3")
@@ -29,3 +31,18 @@ def test_str_command():
 def test_str_wrapper():
     wrapper = FunctionWrapper(name="test", execute=None, required_args=["one", "two"], optional_args=["three"])
     assert str(wrapper) == f"`{COMMAND_PREFIX}test <one> <two> [three]`"
+
+def test_help_embed():
+    wrapper = FunctionWrapper(
+        name="test", execute=None, required_args=["one", "two"], optional_args=["three"],
+        description="A command that uses `<one>`, `<two>`, and `[three]` to do stuff.",
+        example_args=["one two", "1 2 3", "\"o n e\" two three"])
+    registered_commands["test"] = wrapper
+    embed = help_embed("test")
+
+    assert embed
+    assert embed.fields
+    assert embed.fields[0].name == f"`{COMMAND_PREFIX}test <one> <two> [three]`"
+    assert wrapper.description in embed.fields[0].value
+    for example_args in wrapper.example_args:
+        assert f"`{COMMAND_PREFIX}test {example_args}`" in embed.fields[0].value
