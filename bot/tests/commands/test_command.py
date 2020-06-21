bb5c76158bab6bf701f44234e920d51c9d67fb5b
Naxesss <naxess.w@gmail.com>
2020-06-21 12:59:50 +0200

    Add FunctionWrapper __str__ test

--- bot/tests/commands/test_command.py
+++ bot/tests/commands/test_command.py
@@ -1,8 +1,8 @@
 import sys
 sys.path.append('..')
 
-from bot.commands import Command
 from bot.commands import COMMAND_PREFIX
+from bot.commands import Command, FunctionWrapper
 
 def test_init_command():
     command = Command("test", "1", "2", "3")
@@ -25,3 +25,7 @@ def test_hash_command():
 def test_str_command():
     command = Command("test", "1", "2", "3")
     assert str(command) == f"{COMMAND_PREFIX}test 1 2 3"
+
+def test_str_wrapper():
+    wrapper = FunctionWrapper(name="test", execute=None, required_args=["one", "two"], optional_args=["three"])
+    assert str(wrapper) == f"`{COMMAND_PREFIX}test <one> <two> [three]`"
