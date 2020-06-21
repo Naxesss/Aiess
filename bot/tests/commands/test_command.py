8e980fd0960124afd1427c44a702f4d3fed898c4
Naxesss <naxess.w@gmail.com>
2020-06-21 12:52:53 +0200

    Reformat existing command tests
    More specific and less space consuming.

--- bot/tests/commands/test_command.py
+++ bot/tests/commands/test_command.py
@@ -3,14 +3,12 @@ sys.path.append('..')
 
 from bot.commands import Command
 
-def test_init():
+def test_init_command():
     command = Command("test", "1", "2", "3")
-    
     assert command.name == "test"
     assert command.args == ["1", "2", "3"]
 
-def test_init_no_args():
+def test_init_command_no_args():
     command = Command("test")
-
     assert command.name == "test"
     assert not command.args
