c58829335de57f560a4d1b6cc0c9d56c551441c8
Naxesss <naxess.w@gmail.com>
2020-06-23 13:17:26 +0200

    Add cmd_ping description

--- bot/cmd_modules/cmd_ping.py
+++ bot/cmd_modules/cmd_ping.py
@@ -3,6 +3,9 @@ sys.path.append('..')
 
 from bot.commands import Command, register
 
-@register("ping")
+@register(
+    name        = "ping",
+    description = "Returns \"pong\"."
+)
 async def cmd_ping(command: Command):
     await command.respond("pong")
