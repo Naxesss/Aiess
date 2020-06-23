fe929114ca07a79e8683733b3a71f20436e38b7c
Naxesss <naxess.w@gmail.com>
2020-06-23 15:31:54 +0200

    Add command response tests
    Tests for responses of specific commands exist, but no general response tests.

--- bot/tests/commands/test_command.py
+++ bot/tests/commands/test_command.py
@@ -1,6 +1,12 @@
 import sys
 sys.path.append('..')
 
+import pytest
+
+from discord import Embed
+
+from bot.tests.commands.mock_command import MockChannel, MockMessage
+
 from bot.commands import COMMAND_PREFIX
 from bot.commands import registered_commands
 from bot.commands import Command, FunctionWrapper
@@ -31,6 +37,37 @@ def test_str_command():
 def test_str_wrapper():
     wrapper = FunctionWrapper(name="test", execute=None, required_args=["one", "two"], optional_args=["three"])
     assert str(wrapper) == f"`{COMMAND_PREFIX}test <one> <two> [three]`"
+
+
+
+@pytest.mark.asyncio
+async def test_command_respond():
+    mock_channel = MockChannel()
+    mock_message = MockMessage("+test 1 2 3", channel=mock_channel)
+    command = Command("test", "1", "2", "3", context=mock_message)
+
+    assert await command.respond("success")
+    assert command.response == "success"
+    assert command.response_embed is None
+    assert mock_channel.messages[0].content == "success"
+    assert mock_channel.messages[0].embed is None
+
+@pytest.mark.asyncio
+async def test_command_respond_embed():
+    mock_channel = MockChannel()
+    mock_message = MockMessage("+test 1 2 3", channel=mock_channel)
+    command = Command("test", "1", "2", "3", context=mock_message)
+
+    embed = Embed()
+    embed.add_field(name="test", value="success")
+
+    assert await command.respond("", embed=embed)
+    assert command.response == ""
+    assert command.response_embed == embed
+    assert mock_channel.messages[0].content == ""
+    assert mock_channel.messages[0].embed == embed
+
+
 
 def test_help_embed():
     wrapper = FunctionWrapper(
