34f949faebb8c8f9033576cda788769b9c5e4f7b
Naxesss <naxess.w@gmail.com>
2020-06-29 17:04:15 +0200

    Add cmd_recent for obtaining recent events
    This makes use of the other changes in this commit batch to enable this functionality, in an efficient and safe manner.

---
+++ bot/cmd_modules/cmd_recent.py
@@ -0,0 +1,48 @@
+import sys
+sys.path.append('..')
+
+from discord import Embed
+
+from bot.commands import Command, register
+from bot.filterer import passes_filter, filter_to_sql
+from bot.cmdcommon import validate_filter
+from bot.formatter import format_link, format_embed, format_timeago
+
+from bot.database import Database
+from aiess.database import SCRAPER_DB_NAME
+
+@register(
+    name          = "recent",
+    optional_args = ["filter"],
+    description   = "Returns the most recent event gathered, optionally matching `[filter]`.",
+    example_args  = [
+        "type:(rank or love)",
+        "user:\"space in name\"",
+        "(user or creator):someone and not type:reply"
+    ]
+)
+async def cmd_recent(command: Command, _filter: str=None):
+    if _filter and not await validate_filter(command, _filter):
+        return  # `validate_filter` will respond for us.
+
+    filter_query, filter_values = filter_to_sql(_filter)
+    database = Database(SCRAPER_DB_NAME)
+    event = database.retrieve_event_extensive(
+        where        = f"""
+            {filter_query}
+            ORDER BY time DESC
+            LIMIT 1
+            """,
+        where_values = filter_values
+    )
+    
+    matching_filter_str = f" matching `{_filter}`" if _filter else ""
+
+    if not event:
+        await command.respond_err(f"No event{matching_filter_str} could be found.")
+        return
+    
+    await command.respond(
+        response = f"✓ Most recent event{matching_filter_str}\r\n{format_link(event)}",
+        embed = await format_embed(event)
+    )
