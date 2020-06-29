4dc9c9e36a35266fb689110544ec7b7a384f6b1b
Naxesss <naxess.w@gmail.com>
2020-06-29 14:57:19 +0200

    Include expansion in validate_filter response
    This often helps to figure out what went wrong with the given filter when expanding (e.g. for incorrect expansions, or simply using the wrong gates after a NOT).

--- bot/cmdcommon.py
+++ bot/cmdcommon.py
@@ -8,7 +8,7 @@ async def validate_filter(command: Command, _filter: str):
     """Returns whether the filter was considered valid. If invalid, an appropriate response is sent
     where the command was called."""
     try:
-        expand(_filter)
+        expansion = expand(_filter)
     except ValueError as err:
         # E.g. parenthesis inequality.
         await command.respond_err(f"{str(err)}")
@@ -17,20 +17,20 @@ async def validate_filter(command: Command, _filter: str):
     invalid_keys = set(get_invalid_keys(_filter))
     if invalid_keys:
         invalids_formatted = "`" + "`, `".join(invalid_keys) + "`"
-        await command.respond_err(f"Invalid key(s) {invalids_formatted}.")
+        await command.respond_err(f"Invalid key(s) {invalids_formatted} in expansion `{expansion}`.")
         return False
 
     invalid_filters = set(get_invalid_filters(_filter))
     if invalid_filters:
         invalids_strs = (f"{key}:{value}" for key, value in invalid_filters)
         invalids_formatted = "`" + "`, `".join(invalids_strs) + "`"
-        await command.respond_err(f"Invalid value(s) for key(s) {invalids_formatted}.") # TODO: Show valid values.
+        await command.respond_err(f"Invalid value(s) for key(s) {invalids_formatted} in expansion `{expansion}`.") # TODO: Show valid values.
         return False
 
     invalid_words = set(get_invalid_words(_filter))
     if invalid_words:
         invalids_formatted = "`" + "`, `".join(invalid_words) + "`"
-        await command.respond_err(f"Invalid word(s) {invalids_formatted}.")
+        await command.respond_err(f"Invalid word(s) {invalids_formatted} in expansion `{expansion}`.")
         return False
 
     if not hasattr(command.context.channel, "guild"):
