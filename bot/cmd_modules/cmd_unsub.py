e7edf4c604461a2927edbe62d51383c5a40341c4
Naxesss <naxess.w@gmail.com>
2020-07-11 17:20:10 +0200

    Use an embed for unsubscriptions
    This shows what was unsubscribed from as well, so the user can be more certain that they unsubscribed what they wanted.

--- bot/cmd_modules/cmd_unsub.py
+++ bot/cmd_modules/cmd_unsub.py
@@ -1,9 +1,14 @@
 import sys
 sys.path.append('..')
 
+from discord import Embed
+from discord import Colour
+
 from bot.commands import Command, register
 from bot import subscriber
 from bot.subscriber import unsubscribe
+from bot.formatter import escape_markdown
+from bot.filterer import expand
 
 @register(
     name         = "unsub",
@@ -16,4 +21,15 @@ async def cmd_unsub(command: Command):
         return
 
     unsubscribe(subscription)
-    await command.respond("✓ Unsubscribed")
+
+    embed = Embed()
+    embed.colour = Colour.from_rgb(255, 170, 50)
+    embed.add_field(
+        name="🔕\u2000Unsubscribed from",
+        value=f"""
+            {escape_markdown(subscription.filter)}
+            `{expand(subscription.filter)}`
+            """
+    )
+
+    await command.respond("✓", embed=embed)
