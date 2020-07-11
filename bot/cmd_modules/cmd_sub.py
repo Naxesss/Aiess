cc767b4c7e8073a7d179cffc8e5c69c18177cdec
Naxesss <naxess.w@gmail.com>
2020-07-11 19:47:52 +0200

    Show current sub if no arg in cmd_sub
    Figure this is the best way to implement this functionality. If the user expects something to happen calling just +sub with no args, then something should happen, and this fits well for that.

--- bot/cmd_modules/cmd_sub.py
+++ bot/cmd_modules/cmd_sub.py
@@ -6,15 +6,16 @@ from discord import Embed, Colour
 from bot.commands import Command, register
 from bot.commands import COMMAND_PREFIX
 from bot.cmdcommon import validate_filter
+from bot.subscriber import get_subscription
 from bot.subscriber import subscribe
 from bot.filterer import expand
 from bot.formatter import escape_markdown
 
 @register(
     name          = "sub",
-    required_args = ["filter"],
+    optional_args = ["filter"],
     description   = f"""
-        Subscribes this channel to events matching `<filter>`.
+        Subscribes this channel to events matching `[filter]`, if specified, otherwise shows the current channel subscription.
 
         See also `{COMMAND_PREFIX}unsub`.
         """,
@@ -23,7 +24,22 @@ from bot.formatter import escape_markdown
         "user:\"space in name\"",
         "(user or creator):someone and not type:reply"
     ])
-async def cmd_sub(command: Command, _filter: str):
+async def cmd_sub(command: Command, _filter: str=None):
+    if not _filter:
+        subscription = get_subscription(command.context.channel)
+
+        embed = Embed()
+        embed.colour = Colour.from_rgb(255, 170, 50)
+        embed.add_field(
+            name="🔔\u2000Current Subscription",
+            value=f"""
+                {escape_markdown(subscription.filter)}
+                `{expand(subscription.filter)}`
+                """
+        )
+
+        await command.respond("", embed=embed)
+        return
 
     if not await validate_filter(command, _filter):
         return  # `validate_filter` will respond for us.
