34942f4da1f01a253c4e49637ad226783cb9e203
Naxesss <naxess.w@gmail.com>
2020-07-23 15:50:57 +0200

    Add news_parser.py
    Unlike other parser modules, this does not depend on other parser functionality, hence why we're not using a singleton here (no use for inheritance).

---
+++ scraper/parsers/news_parser.py
@@ -0,0 +1,43 @@
+import sys
+sys.path.append('..')
+
+from typing import Generator, Iterator
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+import json
+
+from aiess.objects import Event, User, NewsPost
+from aiess.timestamp import from_string
+
+def parse(events: BeautifulSoup) -> Generator[Event, None, None]:
+    """Returns a generator of news events from the given /news BeautifulSoup response, parsed top-down."""
+    json_index = events.find("script", {"id": "json-index"})
+    if not json_index:
+        raise ValueError("No news json could be found.")
+
+    post_jsons = json.loads(json_index.string)["news_posts"]
+    return parse_post_jsons(post_jsons)
+
+def parse_post_jsons(post_jsons: Iterator[object]) -> Generator[Event, None, None]:
+    """Returns a generator of news events representing the given news post json objects."""
+    for post_json in post_jsons:
+        yield parse_post_json(post_json)
+
+def parse_post_json(post_json: object) -> Event:
+    """Returns an event representing the given news post json object
+    (a single news post instance, for multiple see `parse_post_jsons`)."""
+    author = User(name=post_json["author"].strip())
+    return Event(
+        _type      = "news",
+        time       = from_string(post_json["published_at"]),
+        newspost   = NewsPost(
+            _id       = post_json["id"],
+            title     = post_json["title"],
+            preview   = post_json["preview"],
+            author    = author,
+            slug      = post_json["slug"],
+            image_url = post_json["first_image"]
+        ),
+        user       = author,  # Authors seem to have a space prefixed.
+        content    = post_json["preview"]
+    )
