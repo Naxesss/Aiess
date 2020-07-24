76bd2ecca83cddaa2d92a9d3a1008ac4080f2d83
Naxesss <naxess.w@gmail.com>
2020-07-24 17:35:46 +0200

    Add requester news events test

--- scraper/tests/test_requester.py
+++ scraper/tests/test_requester.py
@@ -1,9 +1,23 @@
 import sys
 sys.path.append('..')
 
+from datetime import datetime
+
+from scraper.requester import get_news_events
 from scraper.requester import get_beatmapset_events
 from scraper.requester import get_discussion_events
 from scraper.requester import get_reply_events
+
+def test_get_news_events():
+    events = get_news_events(_from=datetime.utcnow(), limit=20)
+    
+    event_n = 0
+    for event in events:
+        assert event.user
+        assert event.newspost
+        event_n += 1
+    
+    assert event_n == 20
 
 def test_get_beatmapset_events():
     events = get_beatmapset_events(page=1, limit=50)
