543a2ce9101a779207a9a0ced2ca6020d85d0c88
Naxesss <naxess.w@gmail.com>
2020-07-29 17:32:23 +0200

    Add requester group test

--- scraper/tests/test_requester.py
+++ scraper/tests/test_requester.py
@@ -1,12 +1,32 @@
 import sys
 sys.path.append('..')
 
+import mock
 from datetime import datetime
 
+from aiess.database import Database, SCRAPER_TEST_DB_NAME
+from aiess import event_types as types
+
 from scraper.requester import get_news_events
+from scraper.requester import get_group_events
 from scraper.requester import get_beatmapset_events
 from scraper.requester import get_discussion_events
 from scraper.requester import get_reply_events
+
+def test_get_group_events():
+    Database(SCRAPER_TEST_DB_NAME).clear_table_data("group_users")
+
+    with mock.patch("scraper.parsers.group_parser.SCRAPER_DB_NAME", SCRAPER_TEST_DB_NAME):
+        events = get_group_events(_from=datetime.utcnow())
+
+        event_n = 0
+        for event in events:
+            assert event.type == types.ADD
+            assert event.user
+            assert event.group
+            event_n += 1
+    
+    assert event_n > 100
 
 def test_get_news_events():
     events = get_news_events(_from=datetime.utcnow(), limit=20)
