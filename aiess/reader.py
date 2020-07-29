ae5508be6ab0575571443c8f15e160de51f0bfd1
Naxesss <naxess.w@gmail.com>
2020-07-30 01:15:22 +0200

    Use ADD and REMOVE instead of past tense constants

--- aiess/reader.py
+++ aiess/reader.py
@@ -56,7 +56,7 @@ class Reader():
 
     async def __push_all_new_events(self) -> None:
         """Triggers the on_event method for each new event since the last stored datetime for each scope."""
-        await self.__push_new_events(Scope("mapset", lambda event: event.type != types.NEWS and event.type not in [types.ADDED, types.REMOVED]))
+        await self.__push_new_events(Scope("mapset", lambda event: event.type != types.NEWS and event.type not in [types.ADD, types.REMOVE]))
         await self.__push_new_events(Scope("news",   lambda event: event.type == types.NEWS))
         await self.__push_new_events(Scope("groups", lambda event: event.type in [types.ADDED, types.REMOVED]))
 
