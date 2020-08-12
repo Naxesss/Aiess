ad6792cd306eb248ce56de455e8deb8df892e7bc
Naxesss <naxess.w@gmail.com>
2020-08-12 17:57:48 +0200

    Add passing cmd_info

---
+++ bot/cmd_modules/cmd_info.py
@@ -0,0 +1,70 @@
+import sys
+sys.path.append('..')
+
+from discord import Embed
+
+from aiess.database import SCRAPER_DB_NAME
+
+from bot.commands import Command, register
+from bot.commands import GENERAL_CATEGORY
+from bot.database import Database, BOT_DB_NAME
+from bot.formatter import format_timeago
+
+@register(
+    category      = GENERAL_CATEGORY,
+    names         = ["info", "about", "invite", "source"],
+    description   = "Returns general information about the bot."
+)
+async def cmd_info(command: Command, key: str=None):
+    app_info = await command.client.application_info()
+
+    info_embed = Embed()
+    info_embed.set_author(name=command.client.user.name, icon_url=command.client.user.avatar_url)
+    info_embed.description = app_info.description
+
+    created_at      = command.client.user.created_at
+    guilds_n        = len(command.client.guilds)
+    subscriptions_n = retrieve_subscription_count()
+    events_n        = retrieve_event_count()
+    events_today_n  = retrieve_event_count_today()
+    first_event_at  = retrieve_first_event_at()
+
+    info_embed.add_field(name="Created at",     value=f"**{created_at.date()}**\n({format_timeago(created_at)})")
+    info_embed.add_field(name="Author",         value=f"{app_info.owner}")
+    info_embed.add_field(name="Source",         value="https://github.com/Naxesss/Aiess")
+    info_embed.add_field(name="Events",         value=f"**{events_n}** in total, **{events_today_n}** in past 24h")
+    info_embed.add_field(name="Subscriptions",  value=f"**{subscriptions_n}** in total across **{guilds_n}** server" + ("s" if guilds_n != 1 else ""))
+    info_embed.add_field(name="First event at", value=f"**{first_event_at.date()}**\n({format_timeago(first_event_at)})")
+
+    info_embed.set_image(url="https://i.imgur.com/RR3937R.jpg")
+
+    await command.respond(
+        response = f"https://discord.com/api/oauth2/authorize?client_id={app_info.id}&permissions=0&scope=bot",
+        embed    = info_embed
+    )
+
+def retrieve_event_count():
+    return Database(SCRAPER_DB_NAME).retrieve_table_data(
+        table        = "events",
+        selection    = "COUNT(*)"
+    )[0][0]
+
+def retrieve_event_count_today():
+    return Database(SCRAPER_DB_NAME).retrieve_table_data(
+        table        = "events",
+        where        = "time >= NOW() - INTERVAL 24 HOUR",
+        selection    = "COUNT(*)"
+    )[0][0]
+
+def retrieve_subscription_count():
+    return Database(BOT_DB_NAME).retrieve_table_data(
+        table        = "subscriptions",
+        selection    = "COUNT(*)"
+    )[0][0]
+
+def retrieve_first_event_at():
+    return Database(SCRAPER_DB_NAME).retrieve_table_data(
+        table        = "events",
+        where        = "TRUE ORDER BY time ASC LIMIT 1",
+        selection    = "time"
+    )[0][0]
