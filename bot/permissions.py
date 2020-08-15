80f26f70d30d2f5f20493e2f7dc23af81b5f1c92
Naxesss <naxess.w@gmail.com>
2020-08-16 00:32:25 +0200

    Add type hint return values in permissions.py

--- bot/permissions.py
+++ bot/permissions.py
@@ -7,20 +7,20 @@ from bot.database import Database, BOT_DB_NAME
 
 cache = {}  # 2d-dict, `cache[guild_id][command_wrapper.names[0]] = permission_filter`
 
-def load():
+def load() -> None:
     """Loads the guild-specific command permissions from the database into the cache."""
     cache.clear()
     for perm_obj in Database(BOT_DB_NAME).retrieve_permissions():
         cache[perm_obj.guild_id] = { perm_obj.command_name : perm_obj.permission_filter }
 
-def get_permission_filter(guild_id: int, command_wrapper: FunctionWrapper):
+def get_permission_filter(guild_id: int, command_wrapper: FunctionWrapper) -> str:
     """Returns the permission filter used for this command type in this guild, or None if no such filter exists."""
     if guild_id not in cache or command_wrapper.names[0] not in cache[guild_id]:
         return None
     
     return cache[guild_id][command_wrapper.names[0]]
 
-def set_permission_filter(guild_id: int, command_wrapper: FunctionWrapper, permission_filter: str):
+def set_permission_filter(guild_id: int, command_wrapper: FunctionWrapper, permission_filter: str) -> None:
     """Updates the permission filter in this guild for this command to the given value."""
     if permission_filter is None:
         Database(BOT_DB_NAME).delete_permission(guild_id, command_wrapper.names[0])
