99e4ecc323622b5bcb9d17d39f7b70649b8a75a3
Naxesss <naxess.w@gmail.com>
2020-08-18 08:18:25 +0200

    Add get_command_wrappers tests

--- bot/tests/test_cmdcommon.py
+++ bot/tests/test_cmdcommon.py
@@ -3,6 +3,8 @@ sys.path.append('..')
 
 import pytest
 
+from bot.commands import register
+from bot.commands import registered_commands, registered_aliases
 from bot.commands import Command
 from bot.tests.commands.mock_command import MockMessage, MockChannel, MockUser, MockGuild
 from bot.filterers.event_filterer import filter_context
@@ -10,6 +12,7 @@ from bot.filterers.event_filterer import filter_context
 from bot.cmdcommon import validate_filter
 from bot.cmdcommon import filters_embed
 from bot.cmdcommon import filter_embed
+from bot.cmdcommon import get_command_wrappers
 
 @pytest.mark.asyncio
 async def test_validate_filter():
@@ -60,3 +63,28 @@ async def test_validate_filter_invalid_word():
     assert command.response_embed.fields[0].value == embed.fields[0].value
     assert command.response_embed.fields[1].name == embed.fields[1].name
     assert command.response_embed.fields[1].value == embed.fields[1].value
+
+
+
+@pytest.fixture
+def clear_registry():
+    registered_commands.clear()
+    registered_aliases.clear()
+
+def test_get_command_wrappers(clear_registry):
+    register(
+        category=None, names=["recent", "r"]
+    )(None)
+    register(
+        category=None, names=["ping"]
+    )(None)
+    assert len(get_command_wrappers("recent, ping")) == 2
+
+def test_get_command_wrappers_plus(clear_registry):
+    register(
+        category=None, names=["ping"]
+    )(None)
+    assert get_command_wrappers("+ping")
+
+def test_get_command_wrappers_none(clear_registry):
+    assert not get_command_wrappers("recent, ping")
