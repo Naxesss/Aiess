42d35176fa58e2920b02083ad0fa662124fb1d63
Naxesss <naxess.w@gmail.com>
2020-08-18 10:44:27 +0200

    Add cmd_perms.py
    Shows current permission setup.

---
+++ bot/cmd_modules/cmd_perms.py
@@ -0,0 +1,42 @@
+import sys
+sys.path.append('..')
+
+from typing import Optional, List
+
+from bot.prefixes import DEFAULT_PREFIX
+from bot import commands
+from bot.commands import register
+from bot.commands import Command, FunctionWrapper
+from bot.commands import PERMISSIONS_CATEGORY
+from bot.cmdcommon import validate_filter, permissions_embed, get_command_wrappers
+from bot import permissions
+from bot.filterers.perms_filterer import filter_context
+
+@register(
+    category      = PERMISSIONS_CATEGORY,
+    names         = ["perms", "permissions"],
+    optional_args = ["command(s)"],
+    description   = f"""
+        Shows all command permissions in the server. Optionally only `[command(s)]`.
+
+        See also `{DEFAULT_PREFIX}enable` and `{DEFAULT_PREFIX}disable`.
+        """,
+    example_args  = [
+        None,
+        "recent",
+        "\"help, ping, info\""
+    ]
+)
+async def cmd_perms(command: Command, commands_str: Optional[str]=None):
+    if not command.guild_id():
+        await command.respond_err("Permissions unavailable in DM channels.")
+        return
+
+    command_wrappers = get_command_wrappers(commands_str)
+    await command.respond(
+        response = f"See `{command.prefix()}enable` and `{command.prefix()}disable` for changing permissions.",
+        embed    = permissions_embed(
+            guild_id         = command.guild_id(),
+            command_wrappers = command_wrappers
+        )
+    )
