c280921b8560f7019410af3f52ef10aed9a8eb2e
Naxesss <30292137+Naxesss@users.noreply.github.com>
2020-09-27 04:00:40 +0200

    Catch exceptions on presence changes
    Seems the presence randomly stops updating after a while.
    
    This usually happens because the thread encountered an exception, so adding this to investigate.

--- bot/activity.py
+++ bot/activity.py
@@ -5,13 +5,18 @@ import asyncio
 
 from discord import Client, Activity, Game
 
+from aiess.logger import log_err
+
 from bot.prefixes import DEFAULT_PREFIX
 
 async def loop(client: Client) -> None:
-    while True:
-        await client.change_presence(activity=get_activity(client))
-        # Presence updates are ratelimited at 1 update / 15s.
-        await asyncio.sleep(15)
+    try:
+        while True:
+            await client.change_presence(activity=get_activity(client))
+            # Presence updates are ratelimited at 1 update / 15s.
+            await asyncio.sleep(60)
+    except Exception as ex:
+        log_err(f"WARNING | Discord presence raised \"{ex}\"")
 
 def get_activity(client: Client) -> Activity:
     guild_n = len(client.guilds)
