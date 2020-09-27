fc7bba6b7cdd7946a209baf004092cbc6adecc57
Naxesss <30292137+Naxesss@users.noreply.github.com>
2020-09-27 05:37:06 +0200

    Add docstrings to activity.py

--- bot/activity.py
+++ bot/activity.py
@@ -10,6 +10,7 @@ from aiess.logger import log_err
 from bot.prefixes import DEFAULT_PREFIX
 
 async def loop(client: Client) -> None:
+    """Updates the client activity ("Playing" indicator) every minute."""
     try:
         while True:
             await client.change_presence(activity=get_activity(client))
@@ -19,6 +20,7 @@ async def loop(client: Client) -> None:
         log_err(f"WARNING | Discord presence raised \"{ex}\"")
 
 def get_activity(client: Client) -> Activity:
+    """Returns the "Playing +help | x servers" indicator for the bot."""
     guild_n = len(client.guilds)
     return Game(
         f"{DEFAULT_PREFIX}help | " +
