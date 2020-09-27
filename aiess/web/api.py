5b75e2aa8d00618dd1699b64794a1d2de51e3721
Naxesss <30292137+Naxesss@users.noreply.github.com>
2020-09-27 10:30:47 +0200

    Combine osu api caches
    Previously had two caches for beatmapsets and users. This moves the caching from those functions into their common denominator.
    
    With this we no longer need a function to clear the cache, as we can simply call `api.cache.clear()` instead.

--- aiess/web/api.py
+++ aiess/web/api.py
@@ -6,9 +6,15 @@ from urllib.parse import quote
 from aiess.web.ratelimiter import request_with_rate_limit
 from aiess.settings import API_KEY, API_RATE_LIMIT
 
+cache = {}
 def request_api(request_type: str, query: str) -> object:
     """Requests a json object from the v1 osu!api, where the api key is supplied."""
     request = f"https://osu.ppy.sh/api/{request_type}?{query}&k={API_KEY}"
+
+    cache_line = f"/{request_type}?{query}"
+    if cache_line in cache:
+        return cache[cache_line]
+
     response = request_with_rate_limit(request, API_RATE_LIMIT, "api")
     try:
         json_response = json.loads(response.text)
@@ -17,40 +23,23 @@ def request_api(request_type: str, query: str) -> object:
             error_str = json_response["error"]
             raise ValueError(f"The osu! api responded with an error \"{error_str}\"")
 
+        cache[cache_line] = json_response
         return json_response
     except json.decoder.JSONDecodeError:
-        # This happens whenever the response text is empty (e.g. "[]").
+        # The response text is empty (e.g. "[]").
         return None
 
-requested_beatmapsets = defaultdict()
 def request_beatmapset(beatmapset_id: str) -> object:
     """Requests a json object of the given beatmapset id.
     Caches any response gotten until cleared manually."""
-    if beatmapset_id in requested_beatmapsets and requested_beatmapsets[beatmapset_id]:
-        return requested_beatmapsets[beatmapset_id]
-    
     beatmapset_json = request_api("get_beatmaps", f"s={quote(str(beatmapset_id))}")
-    requested_beatmapsets[beatmapset_id] = beatmapset_json
     return beatmapset_json
 
-requested_users = defaultdict()
 def request_user(user_id: str) -> object:
     """Requests a json object of the given user id.
     Caches any response gotten until cleared manually."""
-    if user_id in requested_users and requested_users[user_id]:
-        return requested_users[user_id]
-    
     user_json = request_api("get_user", f"u={quote(str(user_id))}")
     if len(user_json) > 0:
-        requested_users[user_id] = user_json[0]
         return user_json[0]
-    
-    # For when the user does not exist (e.g. restricted).
-    # Do not cache, in case they get unrestricted soon.
+    # The user is restricted.
     return None
-
-def clear_response_cache() -> None:
-    global requested_beatmapsets
-    global requested_users
-    requested_beatmapsets = defaultdict()
-    requested_users = defaultdict()
