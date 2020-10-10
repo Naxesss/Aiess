3771bb6c54e2b9fa5f98be0711b56a6083225c95
Naxesss <30292137+Naxesss@users.noreply.github.com>
2020-10-10 14:59:03 +0200

    Remove unnecessary if statement
    Makes no difference apparently.

--- bot/formatter.py
+++ bot/formatter.py
@@ -102,11 +102,10 @@ async def format_embed(event: Event, skip_timeago_if_recent: bool=False) -> str:
         value  = await format_field_value(event),
         inline = False
     )
-    if format_footer_text(event) is not None:
-        embed.set_footer(
-            text     = format_footer_text(event),
-            icon_url = format_footer_icon_url(event)
-        )
+    embed.set_footer(
+        text     = format_footer_text(event),
+        icon_url = format_footer_icon_url(event)
+    )
     embed.colour = type_props[event.type].colour
     
     # Unlike `set_footer`, providing `Embed.Empty` to thumbnail and image urls will cause a 400 Bad Request error.
