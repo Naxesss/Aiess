4349b637d71e7da9dd7b19cbf97ed0c643d70b84
Naxesss <30292137+Naxesss@users.noreply.github.com>
2020-10-22 04:35:29 +0200

    Correct `ADDED` to `ADD`

--- scraper/populator.py
+++ scraper/populator.py
@@ -31,7 +31,7 @@ async def populate_from_discussion(event: Event) -> None:
 
 async def populate_from_bnsite(event: Event) -> None:
     """Populates the given event using the bnsite API if possible."""
-    if event.type in [types.REMOVE, types.ADDED]:
+    if event.type in [types.REMOVE, types.ADD]:
         # Group removal content should reflect the bnsite removal reason (e.g. Kicked/Resigned)
         event.content = get_group_bnsite_comment(event)
         event.group.mode = get_group_bnsite_mode(event)
