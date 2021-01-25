760bf9636fce87bffeb3212ed4386f647a45cabc
Naxess <30292137+Naxesss@users.noreply.github.com>
2021-01-25 09:33:54 +0100

    Align assignments in event parser

--- scraper/parsers/beatmapset_event_parser.py
+++ scraper/parsers/beatmapset_event_parser.py
@@ -19,7 +19,7 @@ class BeatmapsetEventParser(EventParser):
     def parse(self, events: BeautifulSoup) -> Generator[Event, None, None]:
         """Returns a generator of BeatmapsetEvents, from the given /events BeautifulSoup response, parsed top-down."""
         json_events = events.find("script", {"id": "json-events"})
-        json_users = events.find("script", {"id": "json-users"})
+        json_users  = events.find("script", {"id": "json-users"})
 
         if not json_events or not json_users:
             # In case the jsons for some reason fail to appear, we can always
@@ -35,7 +35,7 @@ class BeatmapsetEventParser(EventParser):
             return
 
         event_jsons = json.loads(json_events.string)
-        user_jsons = json.loads(json_users.string)
+        user_jsons  = json.loads(json_users.string)
 
         for event_json in event_jsons:
             event = self.parse_event_json(event_json, user_jsons)
@@ -86,7 +86,7 @@ class BeatmapsetEventParser(EventParser):
         try:
             # Scrape object data
             _type = event_json["type"]
-            time = timestamp.from_string(event_json["created_at"])
+            time  = timestamp.from_string(event_json["created_at"])
 
             if "beatmapset" not in event_json or not event_json["beatmapset"]:
                 raise DeletedContextError("No beatmapset was found in this event. It was likely deleted.")
@@ -94,15 +94,15 @@ class BeatmapsetEventParser(EventParser):
             beatmapset_id = event_json["beatmapset"]["id"]
             discussion_id = event_json["discussion"]["id"] if "discussion" in event_json and event_json["discussion"] else None
 
-            user_id = event_json["user_id"] if "user_id" in event_json else None
+            user_id   = event_json["user_id"] if "user_id" in event_json else None
             user_json = self.__lookup_user_json(user_id, user_jsons)
             user_name = user_json["username"] if user_json else None
 
             content = None
             if _type in [types.LANGUAGE_EDIT, types.GENRE_EDIT]:
                 # Language/genre edits always have "old" and "new" fields, which no other type has.
-                old = event_json["comment"]["old"]
-                new = event_json["comment"]["new"]
+                old     = event_json["comment"]["old"]
+                new     = event_json["comment"]["new"]
                 content = f"{old} -> {new}"
             
             if _type in [types.UNLOVE]:
@@ -111,18 +111,19 @@ class BeatmapsetEventParser(EventParser):
 
             # Reconstruct objects
             beatmapset = Beatmapset(beatmapset_id)
-            user = User(user_id, user_name) if user_id is not None else None
+            user       = User(user_id, user_name) if user_id is not None else None
             discussion = Discussion(discussion_id, beatmapset) if discussion_id is not None else None
         except DeletedContextError as err:
             log_err(err)
         else:
             return Event(
-                _type = _type,
-                time = time,
+                _type      = _type,
+                time       = time,
                 beatmapset = beatmapset,
                 discussion = discussion,
-                user = user,
-                content = content)
+                user       = user,
+                content    = content
+            )
         
         return None
     
