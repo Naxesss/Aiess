ebf33f1c1f0e9b48e1231f4503f9a628872fb54c
Naxess <30292137+Naxesss@users.noreply.github.com>
2022-04-16 19:35:55 +0200

    Initialize logger before use in bnstats

--- bnstats/main.py
+++ bnstats/main.py
@@ -7,7 +7,7 @@ import aiess
 from aiess import Event
 from aiess.reader import Reader
 from aiess.database import SCRAPER_DB_NAME
-from aiess.logger import log
+from aiess import logger
 from aiess import event_types as types
 
 from bnstats import interface
@@ -22,9 +22,10 @@ EXPECTED_TYPES = [
 class Reader(aiess.Reader):
     async def on_event(self, event: Event):
         if event.type in EXPECTED_TYPES and (not event.user or event.user.id != 3):
-            log(event, postfix=self.reader_id)
+            logger.log(event, postfix=self.reader_id)
             interface.insert_event(event)
 
+logger.init()
 reader = Reader("bnstats", db_name=SCRAPER_DB_NAME)
 loop = asyncio.get_event_loop()
 loop.run_until_complete(reader.run())
