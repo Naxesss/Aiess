5978010fa186f28ad42fd774c494e5ed42a3a582
Naxess <30292137+Naxesss@users.noreply.github.com>
2023-09-14 01:25:19 +0200

    Send more data to bnplanner interface

--- bnplanner/interface.py
+++ bnplanner/interface.py
@@ -12,32 +12,46 @@ class StatusDocument():
     def __init__(self, event):
         self.time         = (event.time - datetime(1970,1,1)).total_seconds()
         self.beatmapSetId = event.beatmapset.id
+        self.artist       = event.beatmapset.artist
+        self.title        = event.beatmapset.title
+        self.creatorId    = event.beatmapset.creator.id
+        self.creatorName  = event.beatmapset.creator.name
         self.userId       = event.user.id if event.user else None
-        self.status       = to_status_id(event.type)
+        self.userName     = event.user.name if event.user else None
+        self.status       = status_by_event_type[event.type]
+        self.modes        = list(map(to_api_mode, event.beatmapset.modes))
 
 class GroupChangeDocument():
     def __init__(self, event):
-        self.time    = (event.time - datetime(1970,1,1)).total_seconds()
-        self.type    = event.type  # "add" / "remove"
-        self.userId  = event.user.id if event.user else None
-        self.groupId = event.group_id
+        self.time      = (event.time - datetime(1970,1,1)).total_seconds()
+        self.type      = event.type  # "add" / "remove"
+        self.userId    = event.user.id if event.user else None
+        self.userName  = event.user.name if event.user else None
+        self.groupId   = event.group.id
+        self.groupMode = to_api_mode(event.group.mode)
 
-status_id_type_map = {
-    types.QUALIFY:    1,
-    types.NOMINATE:   2,
-    types.DISQUALIFY: 3,
-    types.RESET:      4,
-    types.RANK:       6
+status_by_event_type = {
+    types.QUALIFY:    "Qualified",
+    types.NOMINATE:   "Nominated",
+    types.DISQUALIFY: "Disqualified",
+    types.RESET:      "Reset",
+    types.RANK:       "Ranked"
 }
 
-def to_status_id(type: str) -> int:
-    """Returns the status ID assoicated with the given event type (e.g. qualified = 1)."""
-    return status_id_type_map[type]
+# Greaper wants the mode names the api uses (`catch` -> `fruits`).
+def to_api_mode(mode: str):
+    return mode if mode != "catch" else "fruits"
 
 def insert_event(event: Event) -> None:
     """Sends a POST request with this event as a json, outlined in `Document`."""
-    url = "https://bnplannerbackend.greaper.net/v1/beatmap/add/event/aiess"
-    document = GroupChangeDocument(event) if event.type in ["add", "remove"] else StatusDocument(event)
+    is_group_change = event.type in ["add", "remove"]
+    if is_group_change:
+        url = "https://bnplannerbackend.greaper.net/v2/aiess/event/user"
+        document = GroupChangeDocument(event)
+    else:
+        url = "https://bnplannerbackend.greaper.net/v2/aiess/event/beatmap"
+        document = StatusDocument(event)
+    
     response = requests.post(url, json=vars(document), headers=BNPLANNER_HEADERS)
     # Response codes are the usual ones.
     code = str(response.status_code)
