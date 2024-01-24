f0a9aa78258cfbc10a492bf9a6ec5085bc0b2991
Naxess <30292137+Naxesss@users.noreply.github.com>
2024-01-24 03:53:28 +0100

    Rate limit the bot's channel.send
    For some reason this, at high traffic hours, would occasionally rate limit my discord, making it impossible to use for a few hours.
    
    Not sure if this would fix it, but let's see.

--- aiess/web/ratelimiter.py
+++ aiess/web/ratelimiter.py
@@ -4,40 +4,71 @@ from typing import Dict
 from time import sleep
 from datetime import datetime, timedelta
 from collections import defaultdict
+import asyncio
 
 from aiess.logger import log_err, log
 
 next_request_time: Dict[str, datetime] = defaultdict(datetime.utcnow)
 failed_attempts: Dict[str, datetime] = defaultdict(int)
 
-def invalid_response(response: Response) -> bool:
-    return response is None or str(response.status_code).startswith('5')
-
-def request_with_rate_limit(request_url: str, rate_limit: float, rate_limit_id: str=None, method: str="GET", sleep_if_ratelimited: bool=True, **kwargs) -> Response:
-    """Requests a response object at most once every rate_limit seconds for the same rate_limit_id (default None).
-    Additional keyword arguments are given to the request function (e.g. headers, timeout, etc)."""
+async def async_call_with_rate_limit(awaited_result_func, is_result_invalid, rate_limit: float, rate_limit_id: str=None, sleep_if_ratelimited: bool=True) -> Response:
+    """Calls `result_func` at most once every `rate_limit` seconds for the same `rate_limit_id` (default None).
+    If given an invalid result, backs off exponentially; waiting longer and longer between attempts."""
     global next_request_time
     
-    response = None
-    while invalid_response(response):
+    result = None
+    while is_result_invalid(result):
+        request_time = next_request_time[rate_limit_id]
+        if request_time and request_time > datetime.now():
+            if not sleep_if_ratelimited:
+                return None
+            asyncio.sleep((request_time - datetime.now()).total_seconds())
+
+        result = await awaited_result_func()
+        next_request_time[rate_limit_id] = datetime.now() + timedelta(seconds=rate_limit)
+
+        if is_result_invalid(result):
+            back_off(rate_limit_id)
+
+    if rate_limit_id in failed_attempts:
+        failed_attempts[rate_limit_id] = 0
+
+    return result
+
+def call_with_rate_limit(result_func, is_result_invalid, rate_limit: float, rate_limit_id: str=None, sleep_if_ratelimited: bool=True) -> Response:
+    """Calls `result_func` at most once every `rate_limit` seconds for the same `rate_limit_id` (default None).
+    If given an invalid result, backs off exponentially; waiting longer and longer between attempts."""
+    global next_request_time
+    
+    result = None
+    while is_result_invalid(result):
         request_time = next_request_time[rate_limit_id]
         if request_time and request_time > datetime.now():
             if not sleep_if_ratelimited:
                 return None
             sleep((request_time - datetime.now()).total_seconds())
 
-        response = try_request(request_url, method=method, **kwargs)
+        result = result_func()
         next_request_time[rate_limit_id] = datetime.now() + timedelta(seconds=rate_limit)
 
-        # `try_request` will return None in case of ConnectionErrors or IUAM.
-        # In these cases we back off and wait until it's over.
-        if invalid_response(response):
+        if is_result_invalid(result):
             back_off(rate_limit_id)
 
     if rate_limit_id in failed_attempts:
         failed_attempts[rate_limit_id] = 0
 
-    return response
+    return result
+
+def request_with_rate_limit(request_url: str, rate_limit: float, rate_limit_id: str=None, method: str="GET", sleep_if_ratelimited: bool=True, **kwargs) -> Response:
+    """Requests a response object at most once every rate_limit seconds for the same rate_limit_id (default None).
+    Additional keyword arguments are given to the request function (e.g. headers, timeout, etc)."""
+    return call_with_rate_limit(
+        result_func          = lambda: try_request(request_url, method=method, **kwargs),
+        is_result_invalid    = invalid_response,
+        rate_limit           = rate_limit,
+        rate_limit_id        = rate_limit_id,
+        sleep_if_ratelimited = sleep_if_ratelimited
+    )
 
 def try_request(request_url: str, method: str="GET", **kwargs) -> Response:
     """Requests a response object and returns it if successful, otherwise None is returned.
@@ -62,6 +93,11 @@ def try_request(request_url: str, method: str="GET", **kwargs) -> Response:
     log(f"RECEIVED {response.status_code}: {response.reason}", postfix="requests")
 
     return response
+
+def invalid_response(response: Response) -> bool:
+    # `try_request` will return None in case of ConnectionErrors or IUAM.
+    # In these cases we back off and wait until it's over.
+    return response is None or str(response.status_code).startswith('5')
 
 def back_off(rate_limit_id: str=None) -> None:
     """Postpones the next request for 30 -> 60 -> 120 -> 240 seconds for 1, 2, 3, and 4+ tries respectively.
--- bot/subscriber.py
+++ bot/subscriber.py
@@ -18,6 +18,8 @@ from bot.database import Database, BOT_DB_NAME
 from bot.formatter import format_embed, format_link
 from bot.filterers.event_filterer import filter_context
 
+from aiess.web.ratelimiter import async_call_with_rate_limit
+
 DEFAULT_DB_NAME = BOT_DB_NAME
 
 cache: List[Subscription] = []
@@ -91,9 +93,14 @@ async def send_event(event: Event, subscription: Subscription, bot: discord.Bot,
 
     while True:
         try:
-            await channel.send(
-                content = format_link(event),
-                embed   = pre_generated_embed
+            await async_call_with_rate_limit(
+                awaited_result_func = lambda: channel.send(
+                    content = format_link(event),
+                    embed   = pre_generated_embed
+                ),
+                is_result_invalid   = lambda result: result is None,
+                rate_limit          = 0,
+                rate_limit_id       = "bot_channel_send"
             )
             break
         except Forbidden as ex:
